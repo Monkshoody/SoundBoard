f7041fdda5d48aad591ef4aaaa07b75437d11d64
Monkshoody <stefan-wuest@oulook.de>
2025-04-30 16:24:25 +0200

    debugging

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  build: {
-    sourcemap: true,
-  },
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
