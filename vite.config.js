efc7f8118c6a22a65a21ab4d1d7340b8cbf53465
Monkshoody <stefan-wuest@outlook.de>
2025-05-04 09:47:54 +0200

    test two for headers alignment in CloudFlare

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy'; 
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_headers',
+          dest: '.' // kopiert in dist/
+        }
+      ]
+    })
+  ],
   server: {
     cors: {
       origin: "https://www.owlbear.rodeo",
