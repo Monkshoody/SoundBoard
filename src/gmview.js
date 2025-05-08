76ae05ae3574b432f20f35cf102f255174d7a5e7
Monkshoody <stefan-wuest@outlook.de>
2025-05-08 20:54:23 +0200

    back to beginning

--- src/gmview.js
+++ src/gmview.js
@@ -1,5 +1,5 @@
 import OBR from "@owlbear-rodeo/sdk";
-import { loadPermissions, savePermissions, saveSoundData, loadSoundData, playSoundForAll, triggerGlobalNotification, loadPermissionsKey } from "./permissions.js";
+import { loadPermissions, savePermissions, saveSoundData, loadSoundData, playSoundForAll, triggerGlobalNotification } from "./permissions.js";
 
 const PERMISSIONS_KEY = "com.soundboard/permissions"; // OwlBear-room Namespace for distributing permissions to sounds
 const SOUND_TRIGGER_KEY = "com.soundboard/sound-trigger"; // OwlBear-room Namespace for distributing audio
@@ -59,8 +59,6 @@ export async function setupGMView(container) {
 // initiate metadata for the OwlBear namespace
   const currentMetadata = await OBR.room.getMetadata();
   soundData = await loadSoundData();
-  let permissionsKey = await loadPermissionsKey();
-  console.log("permissionsKey", permissionsKey);
   if (soundData == []) {
     await OBR.room.setMetadata({
       ... currentMetadata,
@@ -113,9 +111,6 @@ export async function setupGMView(container) {
   // EventListener for the switch
   // if the switch is toggled SOUND_PERMISSION_KEY will be set to true or false accordingly
   checkbox.addEventListener("change", async () => {
-    let newpermissionsKey = await loadPermissionsKey();
-    console.log("permissionsKey", newpermissionsKey);
-    console.log("checkbox.checked", checkbox.checked);
     const currentMetadata = await OBR.room.getMetadata();
     await OBR.room.setMetadata({
       ... currentMetadata,
@@ -503,17 +498,16 @@ export async function setupGMView(container) {
         players.forEach((player) => {
           const playerButton = document.createElement('button');
           playerButton.classList.add('player-button');
-          playerButton.innerText = player;
-
+          playerButton.innerText = player.name;
+        
           playerButton.addEventListener('click', () => {
-            console.log(`you clicked ${player}`);
-            //playSoundForPlayers(sound.audio, player);
+            console.log("you clicked", player.name);
+            // Beispiel-Funktion zum Abspielen für ausgewählten Spieler (und GM)
+            //playSoundForPlayers(sound.audio, player); 
           });
-
+        
           playerSoundButton.appendChild(playerButton);
         });
-
-        soundCard.appendChild(playerSoundButton); 
         soundCard.appendChild(checkboxGroup); 
       }
 
